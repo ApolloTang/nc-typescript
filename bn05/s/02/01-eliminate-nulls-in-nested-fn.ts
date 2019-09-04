f88b31be8eeef4354954acb4aa113f13872c1423
Apollo Tang <tang.apollo@gmail.com>
2019-09-04 01:33:31 -0400

    bn05/s/02/01-eliminate-nulls-in-nested-fn.ts

--- bn05/s/02/01-eliminate-nulls-in-nested-fn.ts
+++ bn05/s/02/01-eliminate-nulls-in-nested-fn.ts
@@ -1,9 +1,19 @@
 
 function broken(name: string | null): string {
   function postfix(epithet: string) {
-    return name.charAt(0) + '.  the ' + epithet; // error, 'name' is possibly null
+    return name.charAt(0) + '.  the ' + epithet // Error, Object is possibly null
   }
-  name = name || "Bob";
-  return postfix("great");
+  name = name || 'Bob'
+  return postfix('great')
 }
+
+
+function fixed(name: string | null): string {
+  const _name = name || 'Bob'
+  function postfix(epithet: string) {
+    return _name.charAt(0) + '.  the ' + epithet
+  }
+  return postfix('great')
+}
+
 
